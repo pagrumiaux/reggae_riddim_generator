790dadb2f0a35aa06976fa9faaa0accb318ff310
pagrumiaux <pierreamaury.grumiaux@gmail.com>
2020-10-20 10:21:01 +0200

    finish primary training.py with basic LSTM

--- training.py
+++ training.py
@@ -6,17 +6,56 @@ Created on Tue Sep 15 18:39:22 2020
 """
 
 
-from tensorflow import keras
+import tensorflow as tf
 from music21 import *
 from dataGenerator import DataGenerator
-
-#%%
+import time
+import datetime
+import matplotlib.pyplot as plt
+#%% train generator
 paramsTrain = {'trackName': 'skank',
-               'encoding' : 'many-hot-close',
-               'batch_size': 4,
-               'timesteps': 16,
-        }
+               'encoding' : 'one-hot',
+               'batch_size': 1,
+               'maxLength': 16,
+               'timesteps': 8,
+               'seqOverlap': 'max',
+               'shuffle': False
+               }
 
-paramsTrain['datasetPath'] = 'C:/Users/RQML4978/Documents/Python/music/dataset/reggae/'
+paramsTrain['datasetPath'] = './dataset/'
 
 trainGenerator = DataGenerator(**paramsTrain)
+
+#%% model
+
+# hyperparameters
+timesteps = trainGenerator.timesteps
+nFeat = trainGenerator.nFeatures
+lstmUnits = 64
+lstmAct = 'tanh'
+outputUnits = nFeat
+dropout = 0.2
+nEpochs = 1
+lr = 10e-3
+
+# optimizer
+Adam = tf.keras.optimizers.Adam(lr)
+
+input0 = tf.keras.layers.Input(shape=(timesteps, nFeat), name='input0')
+lstm0  = tf.keras.layers.LSTM(units=lstmUnits, activation=lstmAct, return_sequences = False, recurrent_dropout = dropout, name='lstm0')(input0)
+output = tf.keras.layers.Dense(units=outputUnits, activation='softmax', name='output')(lstm0)
+
+model = tf.keras.models.Model(inputs=input0, outputs=output)
+print(model.summary())
+
+model.compile(optimizer=Adam, loss='categorical_crossentropy', metrics=['categorical_accuracy'])
+
+#%% training
+date = '_'+datetime.datetime.now().strftime('%Y-%m-%d')
+modelPath = "lstm_64units"
+modelPath = modelPath + date + '.h5'
+
+tic = time.time()
+history = model.fit_generator(generator=trainGenerator, epochs=nEpochs, verbose=1)
+model.save(filepath = modelPath)
+toc = time.time()
